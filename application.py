9332e06c5ed44d5639c9bf7ef17055f398b9aeb6
Mike Lehman <mike.lehman@ondefend.com>
2018-10-24 14:13:44 -0400

    initial commit

---
+++ application.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+from hibp import *
+
+app = Flask(__name__)
+api = Api(app)
+
+class User(Resource):
+
+    def get(self, email):
+        data = getAllBreachesForAccount(email)
+        return data, 200
+
+class Breach(Resource):
+
+    def get(self, name):
+        data = getBreachData(name)
+        return data, 200
+
+api.add_resource(User, "/user/<string:email>")
+api.add_resource(Breach, "/breach/<string:name>")
+
+app.run(debug=True)
---
+++ readme.md
@@ -0,0 +1,15 @@
+API Interface for the Have I Been Pwned site
+
+Runs a simple web service to integrate with the HaveIBeenPwned sqlite3
+
+To run:
+
+pip install -r requirements
+python api.py
+
+
+URLs are:
+
+GET /user/<email address> - Returns breach data for that email.
+
+Get /breach/<name> - Returns info on that breach.
